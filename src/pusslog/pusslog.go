41ff73987eac37619c9549222ae68f19b033cd93
Erik Wallentinsen <erik@the-saints.nl>
2012-10-31 12:54:54 +0100

    Add basic process filter

--- src/pusslog/pusslog.go
+++ src/pusslog/pusslog.go
@@ -15,8 +15,14 @@ var highlight = flag.String("hl", "", "highlight tag/process/package name")     
 var prio = flag.String("prio", "VDIWEF", "priority filter (VERBOSE/DEBUG/INFO/WARNING/ERROR/FATAL)") // TODO
 var minprio = flag.String("minprio", "V", "minimum priority level")                                  // TODO
 
+var pid int
+
 func main() {
 	flag.Parse()
+	if len(*process) > 0 || len(*highlight) > 0 {
+		setPidFilter()
+	}
+
 	loop()
 }
 
@@ -56,10 +62,55 @@ func parseline(l string) {
 }
 
 func logmessage(date string, time string, threadid int, processid int, prio string, tag string, message string) {
+	// process id filter (if enabled)
+	if pid > 0 && pid != processid {
+		return
+	}
+
+	// highlight (if enabled)
 	var pre, post string
 	if tag == *highlight {
 		pre = BgYellow + FgBlack
 		post = Reset
 	}
+
+	// Print logmessage
 	fmt.Printf("%s[%s] %s%s\n", pre, tag, message, post)
+}
+
+func setPidFilter() {
+	var cmd = new(exec.Cmd)
+	if len(*process) > 0 {
+		cmd = exec.Command("adb", "shell", "ps", *process)
+	} else if len(*highlight) > 0 {
+		cmd = exec.Command("adb", "shell", "ps", *highlight)
+	}
+
+	if cmd == nil {
+		return
+	}
+
+	stdout, _ := cmd.StdoutPipe()
+	rd := bufio.NewReader(stdout)
+	if err := cmd.Start(); err != nil {
+		log.Fatal("Buffer Error:", err)
+	}
+
+	// Skip first line
+	_, err := rd.ReadString('\n')
+	if err != nil {
+		log.Fatal("Read Error:", err)
+		return
+	}
+
+	str, err := rd.ReadString('\n')
+	if err != nil {
+		log.Fatal("Read Error:", err)
+		return
+	}
+
+	fields := strings.Fields(str)
+	if len(fields) == 9 {
+		pid, _ = strconv.Atoi(fields[1])
+	}
 }
